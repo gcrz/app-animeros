bb661d9763232d853ada12aa31ff55be17635933
Guilherme Cruz <guilhermecruz001@gmail.com>
2021-06-04 01:23:02 -0300

    GET implementado no remoteDatabase

--- app_animeros/lib/data/remote/remote_database.dart
+++ app_animeros/lib/data/remote/remote_database.dart
@@ -1,3 +1,4 @@
+import 'package:app_animeros/model/profile_info.dart';
 import 'package:dio/dio.dart';
 
 class DatabaseRemoteServer {
@@ -8,4 +9,32 @@ class DatabaseRemoteServer {
 
   Dio _dio = Dio();
 
+  Future<List<dynamic>> getProfileList() async {
+    Response response = await _dio.request(this.databaseUrl,
+        options:
+            Options(method: "GET", headers: {"Accept": "application/json"}));
+
+    List<ProfileInfo> profileList = [];
+    List<int> idList = [];
+
+    response.data.forEach((element) {
+      ProfileInfo profileInfo = ProfileInfo.fromMap(element);
+      profileList.add(profileInfo);
+      idList.add(element["id"]);
+    });
+
+    return [profileList, idList];
+  }
+}
+
+void main() async {
+  DatabaseRemoteServer profileServer = DatabaseRemoteServer._createInstance();
+  var response = await profileServer.getProfileList();
+  // 1° [0] se refere à lista de usuários cadastrado
+  // 2° [0] se refere a posição do aluno cadastrado na lista
+  ProfileInfo profileInfo = response[0][0];
+
+  print(profileInfo.username);
+  print(profileInfo.email);
+  print(profileInfo.password);
 }
